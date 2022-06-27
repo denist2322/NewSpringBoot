296fe37ba3053df4af44737419a58c417cee24d0
denist2322 <parksomang125@gmail.com>
2022-06-27 17:19:29 +0900

    이름과 이메일 중복 체크

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -10,30 +10,37 @@ import com.example.demo.vo.Member;
 @Mapper
 public interface MemberRepository {
 	@Insert("""
-			INSERT INTO `member` SET 
+			INSERT INTO `member` SET
 			regDate = NOW(),
-		    updateDate = NOW(),
-		    loginId = #{loginId},
-		    loginPw = #{loginPw},
-		    `name` = #{name},
-		    nickname = #{nickname},
-		    cellphoneNo = #{cellphoneNo},
-		    email = #{email};
+			   updateDate = NOW(),
+			   loginId = #{loginId},
+			   loginPw = #{loginPw},
+			   `name` = #{name},
+			   nickname = #{nickname},
+			   cellphoneNo = #{cellphoneNo},
+			   email = #{email};
 			""")
 	void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name,
 			@Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
-	
+
 	@Select("SELECT LAST_INSERT_ID()")
 	int getLastInsertId();
-	
+
 	@Select("""
 			SELECT * FROM `member` AS M WHERE M.id = #{id}
 			""")
 	Member getMemberById(@Param("id") int id);
-	
+
 	@Select("""
 			SELECT * FROM `member` AS M WHERE M.loginId = #{loginId}
 			""")
 	Member getMemberByLoginId(@Param("loginId") String loginId);
+
+	@Select("""
+			SELECT * FROM `member` AS M
+			WHERE M.name = #{name}
+			AND M.email = #{email}
+			""")
+	Member getMemberByNameAndEmail(@Param("name") String name, @Param("email") String email);
 
 }
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -8,22 +8,33 @@ import com.example.demo.vo.Member;
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
-		this.memberRepository = memberRepository;		
+		this.memberRepository = memberRepository;
 	}
-	
+
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		// 로그인 아이디 중복체크
 		Member oldmember = getMemberByLoginId(loginId);
-		
-		if(oldmember != null) {
+
+		if (oldmember != null) {
 			return -1;
 		}
-		
+
+		// 이름 + 이메일 중복 체크
+		oldmember = getMemberByNameAndEmail(name, email);
+
+		if (oldmember != null) {
+			return -2;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastInsertId();
 	}
 
+	private Member getMemberByNameAndEmail(String name, String email) {
+		return memberRepository.getMemberByNameAndEmail(name, email);
+	}
 
 	private Member getMemberByLoginId(String loginId) {
 		return memberRepository.getMemberByLoginId(loginId);
