6a81340960099eafd841d786d9bcc6bfdfc92540
denist2322 <parksomang125@gmail.com>
2022-06-28 12:23:47 +0900

    로그인기능 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +18,7 @@ public class UsrMemberController {
 	public UsrMemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
-
+	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
@@ -39,17 +41,55 @@ public class UsrMemberController {
 		if (Ut.empty(email)) {
 			return ResultData.from("F-6", "email(을)를 입력해주세요.");
 		}
-		
+
 		// S-1
 		// 회원가입이 완료되었습니다.
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		//알고 싶은거(resultCode, msg)
-		
-		if(joinRd.isFail()) {
-			return (ResultData)joinRd;
+		// 알고 싶은거(resultCode, msg)
+
+		if (joinRd.isFail()) {
+			return (ResultData) joinRd;
 		}
-		
+
 		Member member = memberService.getMemberById(joinRd.getData1());
 		return ResultData.newData(joinRd, member);
 	}
+	
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(HttpSession httpSession,String loginId, String loginPw) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if (isLogined) {
+			return ResultData.from("F-5", "이미 로그인이 되었습니다.");
+		}
+			
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
+		}
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if(member == null) {
+			return ResultData.from("F-3", "존재하지 않는 로그인아이디 입니다.");
+		}
+		
+		if(member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+					
+		}
+		
+		httpSession.setAttribute("loginedMemberId", member.getId());
+		
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
+	}
+	
+	
 }
