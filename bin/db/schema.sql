300a4b4ba3a8005f0bedc1bf567d6523dc4a30ea
denist2322 <parksomang125@gmail.com>
2022-06-29 20:28:39 +0900

    회원번호 칼럼 추가

--- bin/db/schema.sql
+++ bin/db/schema.sql
@@ -77,12 +77,4 @@ INSERT INTO `member` SET
     email = 'dsdss@naver.com';
 
 SELECT * FROM `member`;
-
-# 게시물 테이블에 회원정보 추가
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
-
-SELECT * FROM article;
-
-# 기존 게시물의 작성자를 2번호로 지정
-UPDATE article SET
-memberId = 2 WHERE memberId = 0;
+ 
