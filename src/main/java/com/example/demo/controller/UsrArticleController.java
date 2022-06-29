7da0ae5358675facd1ff4054dd1dbb46a1e5c21c
denist2322 <parksomang125@gmail.com>
2022-06-29 20:52:45 +0900

    로그인 여부 체크와 작성자 정보 저장

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,42 +18,51 @@ import com.example.demo.vo.ResultData;
 public class UsrArticleController {
 	@Autowired
 	private ArticleService articleService;
-	
+
 	@RequestMapping("/usr/article/doadd")
 	@ResponseBody
-	public ResultData<Article> doAdd(String title, String body) {
-		if(Ut.empty(title)) {
+	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+
+		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 입력하세요.");
 		}
-		
-		if(Ut.empty(body)) {
+
+		if (Ut.empty(body)) {
 			return ResultData.from("F-1", "body(을)를 입력하세요.");
 		}
-		
-		
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
+
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 		int id = writeArticleRd.getData1();
-		
+
 		Article article = articleService.getArticle(id);
-				
-		return ResultData.newData(writeArticleRd,article);
+
+		return ResultData.newData(writeArticleRd, article);
 	}
 
-	
-	
 	@RequestMapping("/usr/article/getarticles")
 	@ResponseBody
 	public ResultData<List<Article>> getArticles() {
 		List<Article> article = articleService.getArticles();
-		return ResultData.from("S-1", "게시물 리스트 입니다.",article);
+		return ResultData.from("S-1", "게시물 리스트 입니다.", article);
 	}
 
 	@RequestMapping("/usr/article/getarticle")
 	@ResponseBody
 	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
-		
-		if(article == null) {	
+
+		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
